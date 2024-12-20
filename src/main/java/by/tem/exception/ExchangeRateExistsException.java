d8b5508c270db9d66435d2e2303ec97b22df1dba
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-20 09:15:43 +0300

    feat: add ExchangeRateExistsException extending RuntimeException

---
+++ src/main/java/by/tem/exception/ExchangeRateExistsException.java
@@ -0,0 +1,7 @@
+package by.tem.exception;
+
+public class ExchangeRateExistsException extends RuntimeException {
+    public ExchangeRateExistsException(String message) {
+        super(message);
+    }
+}
