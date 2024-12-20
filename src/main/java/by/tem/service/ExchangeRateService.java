4f0d2a173e70c9baeac066d4c02c4df62854c1a4
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-20 09:16:55 +0300

    feat: add save method to execute saving new exchange rate

--- src/main/java/by/tem/service/ExchangeRateService.java
+++ src/main/java/by/tem/service/ExchangeRateService.java
@@ -3,6 +3,7 @@ package by.tem.service;
 import by.tem.dao.ExchangeRateDao;
 import by.tem.dto.ExchangeRateDto;
 import by.tem.entity.ExchangeRate;
+import by.tem.exception.ExchangeRateExistsException;
 import by.tem.exception.ExchangeRateNotFoundException;
 import by.tem.mapper.ExchangeRateMapper;
 
@@ -11,7 +12,7 @@ import java.util.Optional;
 
 public class ExchangeRateService {
     private static final ExchangeRateService INSTANCE = new ExchangeRateService();
-    private ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
+    private final ExchangeRateDao exchangeRateDao = ExchangeRateDao.getInstance();
 
     public List<ExchangeRateDto> findAll() {
         return ExchangeRateMapper.toDtoList(exchangeRateDao.findAll());
@@ -23,6 +24,21 @@ public class ExchangeRateService {
             return ExchangeRateMapper.toDto(exchangeOptional.get());
         }
         throw new ExchangeRateNotFoundException(String.format("Exchange rate not found for base currency code '%s' and target currency code '%s'", baseCurrencyCode, targetCurrencyCode));
+    }
+
+    public ExchangeRateDto save(ExchangeRateDto exchangeRateDto) {
+        ExchangeRate exchangeRate = ExchangeRateMapper.toExchangeRate(exchangeRateDto);
+        Optional<ExchangeRate> exchangeRateOptional = exchangeRateDao.findByCode(
+                exchangeRate.getBaseCurrency().getCode(),
+                exchangeRate.getTargetCurrency().getCode()
+        );
+        if (exchangeRateOptional.isEmpty()) {
+            ExchangeRate savedExchangeRate = exchangeRateDao.save(exchangeRate);
+            return ExchangeRateMapper.toDto(savedExchangeRate);
+        }
+        throw new ExchangeRateExistsException(
+                "Exchange rate with base currency code " + exchangeRateDto.getBaseCurrency().getCode() +
+                " and target currency code " + exchangeRateDto.getTargetCurrency().getCode() + " already exist.");
     }
 
     public static ExchangeRateService getInstance() {
