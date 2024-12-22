bdb3baecb04ab393e434473320f60bb33e66a030
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-22 21:15:56 +0300

    refactor: replace boilerplate code with Lombok annotations for cleaner classes

--- src/main/java/by/tem/entity/Currency.java
+++ src/main/java/by/tem/entity/Currency.java
@@ -1,75 +1,15 @@
 package by.tem.entity;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Currency {
     private Integer id;
     private String code;
     private String fullName;
     private String sign;
-
-    public Currency(Integer id, String code, String fullName, String sign) {
-        this.id = id;
-        this.code = code;
-        this.fullName = fullName;
-        this.sign = sign;
-    }
-
-    public Currency() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getSign() {
-        return sign;
-    }
-
-    public void setSign(String sign) {
-        this.sign = sign;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        Currency currency = (Currency) object;
-        return Objects.equals(id, currency.id) && Objects.equals(code, currency.code) && Objects.equals(fullName, currency.fullName) && Objects.equals(sign, currency.sign);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, code, fullName, sign);
-    }
-
-    @Override
-    public String toString() {
-        return "Currency{" +
-               "id=" + id +
-               ", code='" + code + '\'' +
-               ", fullName='" + fullName + '\'' +
-               ", sign='" + sign + '\'' +
-               '}';
-    }
 }
--- src/main/java/by/tem/entity/ExchangeRate.java
+++ src/main/java/by/tem/entity/ExchangeRate.java
@@ -1,72 +1,17 @@
 package by.tem.entity;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ExchangeRate {
     private Integer id;
     private Currency baseCurrency;
     private Currency targetCurrency;
-    private Double rate;
-
-    public ExchangeRate(Integer id, Currency baseCurrency, Currency targetCurrency, Double rate) {
-        this.id = id;
-        this.baseCurrency = baseCurrency;
-        this.targetCurrency = targetCurrency;
-        this.rate = rate;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public Currency getBaseCurrency() {
-        return baseCurrency;
-    }
-
-    public void setBaseCurrency(Currency baseCurrency) {
-        this.baseCurrency = baseCurrency;
-    }
-
-    public Currency getTargetCurrency() {
-        return targetCurrency;
-    }
-
-    public void setTargetCurrency(Currency targetCurrency) {
-        this.targetCurrency = targetCurrency;
-    }
-
-    public Double getRate() {
-        return rate;
-    }
-
-    public void setRate(Double rate) {
-        this.rate = rate;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (this == object) return true;
-        if (object == null || getClass() != object.getClass()) return false;
-        ExchangeRate that = (ExchangeRate) object;
-        return Objects.equals(id, that.id) && Objects.equals(baseCurrency, that.baseCurrency) && Objects.equals(targetCurrency, that.targetCurrency) && Objects.equals(rate, that.rate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, baseCurrency, targetCurrency, rate);
-    }
-
-    @Override
-    public String toString() {
-        return "ExchangeRate{" +
-               "id=" + id +
-               ", baseCurrency=" + baseCurrency +
-               ", targetCurrency=" + targetCurrency +
-               ", rate=" + rate +
-               '}';
-    }
+    private BigDecimal rate;
 }
