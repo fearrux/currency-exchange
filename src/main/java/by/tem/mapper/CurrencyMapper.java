157e7d278192e1183468340485beec8b72ae5bd5
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-23 14:32:26 +0300

    refactor: make singleton class and remove toDtoList method

--- src/main/java/by/tem/mapper/CurrencyMapper.java
+++ src/main/java/by/tem/mapper/CurrencyMapper.java
@@ -3,31 +3,29 @@ package by.tem.mapper;
 import by.tem.dto.CurrencyDto;
 import by.tem.entity.Currency;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class CurrencyMapper {
-    public static CurrencyDto toDto(Currency currency) {
+    private static final CurrencyMapper INSTANCE = new CurrencyMapper();
+
+    public CurrencyDto toDto(Currency currency) {
         return new CurrencyDto(
                 currency.getId(),
                 currency.getFullName(),
                 currency.getCode(),
-                currency.getSign()
-        );
+                currency.getSign());
     }
 
-    public static Currency toCurrency(CurrencyDto dto) {
-        Currency currency = new Currency();
-        currency.setId(dto.getId());
-        currency.setFullName(dto.getName());
-        currency.setCode(dto.getCode());
-        currency.setSign(dto.getSign());
-        return currency;
+    public Currency toEntity(CurrencyDto currencyDto) {
+        return new Currency(
+                currencyDto.id(),
+                currencyDto.name(),
+                currencyDto.code(),
+                currencyDto.sign());
     }
 
-    public static List<CurrencyDto> toDtoList(List<Currency> currencies) {
-        return currencies.stream()
-                .map(CurrencyMapper::toDto)
-                .collect(Collectors.toList());
+    public static CurrencyMapper getInstance() {
+        return INSTANCE;
+    }
+
+    private CurrencyMapper() {
     }
 }
