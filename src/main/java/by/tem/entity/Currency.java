11d1d9fca68fb4d94cc4c0bfaf95875b0020dca9
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-23 17:17:46 +0300

    refactor: update Currency class fields

--- src/main/java/by/tem/entity/Currency.java
+++ src/main/java/by/tem/entity/Currency.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Currency {
     private Integer id;
+    private String name;
     private String code;
-    private String fullName;
     private String sign;
 }
