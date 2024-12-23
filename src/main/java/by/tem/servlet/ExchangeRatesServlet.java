24e15d4821af3956f82ea81b6363771b015db86d
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-23 17:32:00 +0300

    refactor: move repeated code to a separate method for better readability

--- src/main/java/by/tem/servlet/ExchangeRatesServlet.java
+++ src/main/java/by/tem/servlet/ExchangeRatesServlet.java
@@ -1,17 +1,14 @@
 package by.tem.servlet;
 
-import by.tem.dto.CurrencyDto;
+import by.tem.dto.ErrorResponse;
 import by.tem.dto.ExchangeRateDto;
 import by.tem.exception.CurrencyNotFoundException;
+import by.tem.exception.DatabaseConnectionException;
 import by.tem.exception.ExchangeRateExistsException;
 import by.tem.exception.InvalidDataException;
-import by.tem.mapper.CurrencyMapper;
-import by.tem.service.CurrencyService;
 import by.tem.service.ExchangeRateService;
-import by.tem.validation.CurrencyValidator;
-import by.tem.validation.ExchangeRateValidator;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -24,26 +21,20 @@ import java.util.List;
 
 @WebServlet("/exchangeRates")
 public class ExchangeRatesServlet extends HttpServlet {
-    private static final ExchangeRateService exchangeRateService = ExchangeRateService.getInstance();
+    private final ExchangeRateService exchangeRateService = ExchangeRateService.getInstance();
     private final ObjectMapper objectMapper = new ObjectMapper();
-
-    @Override
-    public void init(ServletConfig config) throws ServletException {
-        super.init(config);
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
         PrintWriter writer = resp.getWriter();
+
         try {
             List<ExchangeRateDto> exchangeRates = exchangeRateService.findAll();
-            String json = objectMapper.writeValueAsString(exchangeRates);
-            writer.write(json);
-        } catch (RuntimeException exception) {
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            writer.write(exception.getMessage());
+            writer.write(objectMapper.writeValueAsString(exchangeRates));
+        } catch (DatabaseConnectionException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), writer);
         } finally {
             writer.close();
         }
@@ -53,41 +44,32 @@ public class ExchangeRatesServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
+        PrintWriter writer = resp.getWriter();
+
         String baseCurrencyCode = req.getParameter("baseCurrencyCode");
         String targetCurrencyCode = req.getParameter("targetCurrencyCode");
         String rate = req.getParameter("rate");
-        PrintWriter writer = resp.getWriter();
+
         try {
-            CurrencyValidator.isValidCode(baseCurrencyCode);
-            CurrencyValidator.isValidCode(targetCurrencyCode);
-            ExchangeRateValidator.isValidRate(rate);
-            ExchangeRateDto exchangeRateDto = new ExchangeRateDto();
-            CurrencyService currencyService = CurrencyService.getInstance();
-//            try to find currency in database with its code
-            CurrencyDto baseCurrencyDto = currencyService.findByCode(baseCurrencyCode);
-            CurrencyDto targetCurrencyDto = currencyService.findByCode(targetCurrencyCode);
-//            set found currency to exchange rate
-            exchangeRateDto.setBaseCurrency(CurrencyMapper.toCurrency(baseCurrencyDto));
-            exchangeRateDto.setTargetCurrency(CurrencyMapper.toCurrency(targetCurrencyDto));
-            exchangeRateDto.setRate(Double.parseDouble(rate));
-            ExchangeRateDto savedExchangeRate = exchangeRateService.save(exchangeRateDto);
+            ExchangeRateDto exchangeRate = exchangeRateService.save(baseCurrencyCode, targetCurrencyCode, rate);
             resp.setStatus(HttpServletResponse.SC_CREATED);
-            String json = objectMapper.writeValueAsString(savedExchangeRate);
-            writer.write(json);
-        } catch (InvalidDataException exception) {
-            resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-            writer.write(exception.getMessage());
-        } catch (ExchangeRateExistsException exception) {
-            resp.setStatus(HttpServletResponse.SC_CONFLICT);
-            writer.write(exception.getMessage());
-        } catch (CurrencyNotFoundException exception) {
-            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
-            writer.write(exception.getMessage());
-        } catch (RuntimeException exception) {
-            resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-            writer.write(exception.getMessage());
+            writer.write(objectMapper.writeValueAsString(exchangeRate));
+        } catch (InvalidDataException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_BAD_REQUEST, e.getMessage(), writer);
+        } catch (ExchangeRateExistsException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_CONFLICT, e.getMessage(), writer);
+        } catch (CurrencyNotFoundException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_NOT_FOUND, e.getMessage(), writer);
+        } catch (DatabaseConnectionException e) {
+            sendErrorResponse(resp, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, e.getMessage(), writer);
         } finally {
             writer.close();
         }
+    }
+
+    private void sendErrorResponse(HttpServletResponse resp, int statusCode, String message, PrintWriter writer) throws JsonProcessingException {
+        resp.setStatus(statusCode);
+        ErrorResponse errorResponse = new ErrorResponse(statusCode, message);
+        writer.write(objectMapper.writeValueAsString(errorResponse));
     }
 }
