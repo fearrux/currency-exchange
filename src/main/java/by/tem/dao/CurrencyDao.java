77ee02181ce8ee556f4af9b4c1ac9bb7f4d84c1b
Tembulat Bekov <tembulatbekov7@gmail.com>
2024-12-23 17:22:12 +0300

    refactor: change name of package of connection pool

--- src/main/java/by/tem/dao/CurrencyDao.java
+++ src/main/java/by/tem/dao/CurrencyDao.java
@@ -1,8 +1,8 @@
 package by.tem.dao;
 
 import by.tem.entity.Currency;
-import by.tem.exception.CurrencyExistsException;
-import by.tem.util.ConnectionManager;
+import by.tem.exception.DatabaseConnectionException;
+import by.tem.util.ConnectionPoolService;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -28,7 +28,7 @@ public class CurrencyDao {
 
     public List<Currency> findAll() {
         List<Currency> currencies = new ArrayList<>();
-        try (Connection connection = ConnectionManager.get();
+        try (Connection connection = ConnectionPoolService.get();
              PreparedStatement preparedStatement = connection.prepareStatement(FIND_ALL_SQL)) {
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
@@ -36,12 +36,12 @@ public class CurrencyDao {
             }
             return currencies;
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseConnectionException("Something went wrong when you tried to connect database.");
         }
     }
 
     public Optional<Currency> findByCode(String code) {
-        try (Connection connection = ConnectionManager.get();
+        try (Connection connection = ConnectionPoolService.get();
              PreparedStatement preparedStatement = connection.prepareStatement(FIND_BY_CODE_SQL)) {
             preparedStatement.setString(1, code);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -51,24 +51,25 @@ public class CurrencyDao {
             }
             return Optional.ofNullable(currency);
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseConnectionException("Something went wrong when you tried to connect database.");
         }
     }
 
     public Currency save(Currency currency) {
-        try (Connection connection = ConnectionManager.get();
+        try (Connection connection = ConnectionPoolService.get();
              PreparedStatement preparedStatement = connection.prepareStatement(SAVE_SQL, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, currency.getCode());
-            preparedStatement.setString(2, currency.getFullName());
+            preparedStatement.setString(2, currency.getName());
             preparedStatement.setString(3, currency.getSign());
             preparedStatement.executeUpdate();
             ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
-                currency.setId(generatedKeys.getInt(1));
+                Integer id = generatedKeys.getInt(1);
+                return new Currency(id, currency.getCode(), currency.getName(), currency.getSign());
             }
-            return currency;
+            throw new SQLException("No ID obtained");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new DatabaseConnectionException("Something went wrong when you tried to connect database.");
         }
     }
 
@@ -85,13 +86,5 @@ public class CurrencyDao {
     }
 
     private CurrencyDao() {
-    }
-
-    public static void main(String[] args) {
-        CurrencyDao instance = CurrencyDao.getInstance();
-        for (Currency c : instance.findAll()) {
-            System.out.println(c);
-        }
-
     }
 }
